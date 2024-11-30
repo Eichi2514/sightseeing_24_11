8846042e71bc9e6e9dc945ef396323bfdce4ddf0
Eichi2514 <picon1317@gmail.com>
2024-11-30 17:13:49 +0900

    지역별로 날씨와 관광지 보기 기능 구현

--- src/main/java/org/eichi/sightseeing/domain/home/home/controller/HomeController.java
+++ src/main/java/org/eichi/sightseeing/domain/home/home/controller/HomeController.java
@@ -1,15 +1,25 @@
 package org.eichi.sightseeing.domain.home.home.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 @Controller
 
 public class HomeController {
 
+    @Value("${openweathermapAPIkey}")
+    String openweathermapAPIkey;
+
+    @Value("${attractionAPIkey}")
+    String attractionAPIkey;
+
     @GetMapping("/")
-    public String ShowMain() {
+    public String ShowMain(Model model) {
+
+        model.addAttribute("openweathermapAPIkey", openweathermapAPIkey);
+        model.addAttribute("attractionAPIkey", attractionAPIkey);
         return "/usr/home/main";
     }
 }
